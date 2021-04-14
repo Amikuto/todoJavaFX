7be6506997730be2d4344ae9c63a794680690fd8
Damir Kurtaev <56434766+amikuto@users.noreply.github.com>
2021-04-14 23:02:46 +0300

    added login and registration scene and simple logic

--- src/ami/Main.java
+++ src/ami/Main.java
@@ -1,14 +1,19 @@
 package ami;
 
 import ami.controllers.MainSceneController;
+import ami.controllers.UserLoginPageController;
+import ami.controllers.UserRegistrationPageController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main extends Application {
 
@@ -18,12 +23,18 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         this.primaryStage = primaryStage;
-//        Parent root = FXMLLoader.load(getClass().getResource("/views/initRootLayout.fxml"));
-        primaryStage.setTitle("TODO application");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
+        this.primaryStage.setTitle("TODO application");
+        this.primaryStage.setMaximized(true);
+
+        InputStream iconStream = getClass().getResourceAsStream("/icons/todo.jpeg");
+        assert iconStream != null;
+        Image image = new Image(iconStream);
+        this.primaryStage.getIcons().add(image);
 
         initRootLayout();
+        if (showLoginPage()) {
+            showMainScene();
+        }
 
     }
 
@@ -43,13 +54,49 @@ public class Main extends Application {
         fxmlLoader.setLocation(Main.class.getResource("/views/MainScene.fxml"));
         AnchorPane mainScene = fxmlLoader.load();
 
-        Scene scene = new Scene(mainScene);
         rootLayout.setCenter(mainScene);
 
         MainSceneController mainSceneController = new MainSceneController();
         fxmlLoader.setController(mainSceneController);
         mainSceneController.setMainApp(this);
+    }
 
+    public boolean showLoginPage() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/views/UserLoginPage.fxml"));
+        AnchorPane loginPage = loader.load();
+
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Вход");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        dialogStage.initOwner(primaryStage);
+        Scene scene = new Scene(loginPage);
+        dialogStage.setScene(scene);
+
+        UserLoginPageController controller = loader.getController();
+        controller.setDialogStage(dialogStage);
+        controller.setMainApp(this);
+
+        dialogStage.showAndWait();
+        return controller.isLoginClicked();
+    }
+
+    public void showRegistrationPage() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/views/UserRegistrationPage.fxml"));
+        AnchorPane registrationPage = loader.load();
+
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Регистрация");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        dialogStage.initOwner(primaryStage);
+        Scene scene = new Scene(registrationPage);
+        dialogStage.setScene(scene);
+
+        UserRegistrationPageController controller = loader.getController();
+        controller.setDialogStage(dialogStage);
+
+        dialogStage.showAndWait();
     }
 
 
--- src/ami/models/Task.java
+++ src/ami/models/Task.java
@@ -1,9 +1,11 @@
 package ami.models;
 
 import javafx.beans.property.*;
+//import lombok.Data;
 
 import java.util.Date;
 
+//@Data
 public class Task {
 
     LongProperty id;
